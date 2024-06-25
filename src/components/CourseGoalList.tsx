b8900cd27dbc19a15cc701f762392be2386fefff
Pavlo Leinonen <leinonenko@gmail.com>
2024-06-25 17:38:44 +0300

    add hint and warning boxes

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,14 +1,34 @@
+import { type CourseGoal as CGoal } from "../App.tsx";
+import { type ReactNode } from "react";
+
+import InfoBox from "./InfoBox.tsx";
 import CourseGoal from "./CourseGoal.tsx";
-import { CourseGoal as CGoal } from "../App.tsx";
 
 type CourseGoalListProps = {
   goals: CGoal[];
   onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no goal yet. Start adding lazy mzf</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">Easy boy! Don't put too much goals!</InfoBox>
+    );
+  }
   return (
     <>
+    {warningBox}
       <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
